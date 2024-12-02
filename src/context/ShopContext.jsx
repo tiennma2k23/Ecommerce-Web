1efb0ef10ba4c73d7fd9c4ea96cceab3e4b262b0
Winter-0101 <quangthien1310@gmail.com>
2024-12-02 15:07:20 +0700

    stash

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -15,7 +15,6 @@ const ShopContextProvider = (props) => {
     const [showSearch,setShowSearch] = useState(false);
     const [cartItems,setCartItems] = useState({});
     const [isAuthenticated, setIsAuthenticated] = useState(false); 
-    const [isAuthenticated, setIsAuthenticated] = useState(false); 
     const navigate = useNavigate();
 
     const addToCart = async (itemId, size) => {
@@ -111,8 +110,6 @@ const ShopContextProvider = (props) => {
         search,setSearch,showSearch,setShowSearch,
         cartItems,addToCart,
         getCartCount,updateQuantity,
-        getCartAmount, navigate, 
-        isAuthenticated, setIsAuthenticated, logout
         getCartAmount, navigate, 
         isAuthenticated, setIsAuthenticated, logout
     }
