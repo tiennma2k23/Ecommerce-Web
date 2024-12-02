fe0b31d0f4389cc2c5f0bf2fe2bfd5a5f91fd996
tiennma2k23 <tien.nm@ascendcorp.com>
2024-12-02 14:03:57 +0700

    change BE

--- BE/ECommerceApplication/src/main/java/com/app/payloads/JWTAuthRequest.java
+++
@@ -1,9 +0,0 @@
-package com.app.payloads;
-
-import lombok.Data;
-
-@Data
-public class JWTAuthRequest {
-	private String username;  // email
-	private String password;
-}
---
+++ BE/src/main/java/com/backend/ecommerce/model/PaymentMethod.java
@@ -0,0 +1,5 @@
+package com.backend.ecommerce.model;
+
+public enum PaymentMethod {
+    CASH_ON_DELIVERY
+}
