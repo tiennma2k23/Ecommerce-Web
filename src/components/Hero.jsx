be3feb93689e8692f128e0304d36f5678d2444d1
Winter-0101 <quangthien1310@gmail.com>
2024-12-14 14:26:02 +0700

    Homepage to vietnamese

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,11 +9,11 @@ const Hero = () => {
             <div className='text-[#414141]'>
                 <div className='flex items-center gap-2'>
                     <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
-                    <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
+                    <p className='font-sans font-medium text-sm md:text-base'>BESTSELLER CỦA CHÚNG TÔI</p>
                 </div>
-                <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
+                <h1 className='font-sans roboto-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'>SẢN PHẨM MỚI</h1>
                 <div className='flex items-center gap-2'>
-                    <p className='font-semibold text-sm  md:text-base'>SHOP NOW</p>
+                    <p className='font-sans font-semibold text-sm  md:text-base'>MUA NGAY</p>
                     <p className='w-8 md:w-11 h-[1px] bg-[#414141]'></p>
                 </div>
             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,19 +16,19 @@ const Navbar = () => {
         <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
 
             <NavLink to='/' className='flex flex-col items-center gap-1'>
-                <p>HOME</p>
+            <p className="font-sans">TRANG CHỦ</p>
                 <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
             </NavLink>
             <NavLink to='/collection' className='flex flex-col items-center gap-1'>
-                <p>COLLECTION</p>
+            <p className="font-sans">BỘ SƯU TẬP</p>
                 <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
             </NavLink>
             <NavLink to='/about' className='flex flex-col items-center gap-1'>
-                <p>ABOUT</p>
+            <p className="font-sans">VỀ CHÚNG TÔI</p>
                 <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
             </NavLink>
             <NavLink to='/contact' className='flex flex-col items-center gap-1'>
-                <p>CONTACT</p>
+            <p className="font-sans">LIÊN HỆ</p>
                 <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
             </NavLink>
 
