5c0c2720e995ae9248d02660519ec83e1f415bcb
Winter-0101 <quangthien1310@gmail.com>
2024-12-14 14:27:29 +0700

    Homepage to vietnamese

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
 
       <div>
           <hr />
-          <p className='py-5 text-sm text-center'>Copyright 2024@ sporter.com - All Right Reserved.</p>
+          <p className='py-5 text-sm text-center'>Bản quyền thuộc về 2024@sporter.com.</p>
       </div>
 
     </div>
