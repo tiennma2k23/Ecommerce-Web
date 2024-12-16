7ff813d8c31b21783c220baee785e158a3c96add
Winter-0101 <quangthien1310@gmail.com>
2024-12-16 16:37:36 +0700

    small modify

--- src/components/RelatedProducts.jsx
+++ src/components/RelatedProducts.jsx
@@ -25,7 +25,7 @@ const RelatedProducts = ({category,subCategory}) => {
   return (
     <div className='my-24'>
       <div className='text-center text-3xl py-2'>
-        <Title text1={'RELATED'} text2={'PRODUCTS'} />
+        <Title text1={'SẢN PHẨM'} text2={'LIÊN QUAN'} />
       </div>
 
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
